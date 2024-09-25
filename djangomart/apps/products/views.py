5d3c8205beece29c3c0b88257df82fcc64fe1667
Al Irfan Alve <alirfanalve89@gmail.com>
2024-09-25 17:01:34 +0600

    edit

--- djangomart/apps/products/views.py
+++ djangomart/apps/products/views.py
@@ -48,14 +48,11 @@ def base(request):
 def admin_view(request):
     return render(request, 'admin_dashboard.html')
 
-def addproduct_view(request):
-    return render(request, 'add_product.html')
-
-def editproduct_view(request):
-    return render(request, 'edit_product.html')
-
 def deleteproduct_view(request):
     return render(request, 'delete_product.html')
+
+def editproduct_view(request):
+    return render(request, 'search_edit_product.html')
 
 def product_details_view(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
@@ -72,4 +69,26 @@ def add_product_view(request):
         form= ProductForm()
     return render(request, 'add_product.html', {'form': form})
 
+def search_and_edit_product(request):
+    product = None  # Initialize product as None
+    
+    if request.method == 'POST':
+        product_id = request.POST.get('product_id')  # Get the product ID from the search form
+        try:
+            product = Product.objects.get(Product_ID=product_id)  # Fetch product by ID
+        except Product.DoesNotExist:
+            product = None  # If the product doesn't exist, set to None
 
+        if product and 'edit_product' in request.POST:
+            # If the edit form is submitted
+            form = ProductForm(request.POST, instance=product)
+            if form.is_valid():
+                form.save()  # Save changes
+                return redirect('search_and_edit_product')  # Redirect back to search and edit page
+        else:
+            # If product exists but form is not submitted yet, pre-fill the form
+            form = ProductForm(instance=product)
+    else:
+        form = None  # If no form submitted yet, form remains None
+
+    return render(request, 'search_edit_product.html', {'form': form, 'product': product})
